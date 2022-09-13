984fc098244a5c02ebe20f3f2778e9fad8277255
Steffen Vogel <post@steffenvogel.de>
2022-09-13 03:27:37 -0400

    updated axidma driver

---
+++ orig/axidma_v9_13/data/axidma_tapp.tcl
@@ -0,0 +1,95 @@
+###############################################################################
+# Copyright (C) 2005 - 2021 Xilinx, Inc.  All rights reserved.
+# SPDX-License-Identifier: MIT
+#
+##############################################################################
+# Uses $XILINX_EDK/bin/lib/xillib_sw.tcl
+
+# -----------------------------------------------------------------
+# Software Project Types (swproj):
+#   0 : MemoryTest - Calls basic  memorytest routines from common driver dir
+#   1 : PeripheralTest - Calls any existing polled_example and/or selftest
+# -----------------------------------------------------------------
+
+# -----------------------------------------------------------------
+# TCL Procedures:
+# -----------------------------------------------------------------
+
+proc gen_include_files {swproj mhsinst} {
+  if {$swproj == 0} {
+    return ""
+  }
+  if {$swproj == 1} {
+    set inc_file_lines {axidma_header.h}
+    return $inc_file_lines
+  }
+}
+
+proc gen_src_files {swproj mhsinst} {
+  if {$swproj == 0} {
+    return ""
+  }
+  if {$swproj == 1} {
+    set inc_file_lines {examples/xaxidma_example_selftest.c data/axidma_header.h}
+    return $inc_file_lines
+  }
+}
+
+proc gen_testfunc_def {swproj mhsinst} {
+  return ""
+}
+
+proc gen_init_code {swproj mhsinst} {
+  return ""
+}
+
+proc gen_testfunc_call {swproj mhsinst} {
+
+  if {$swproj == 0} {
+    return ""
+  }
+
+  set ipname [get_property NAME $mhsinst]
+  set deviceid [::hsi::utils::get_ip_param_name $mhsinst "DEVICE_ID"]
+   set stdout [get_property CONFIG.STDOUT [get_os]]
+   if { $stdout == "" || $stdout == "none" } {
+       set hasStdout 0
+   } else {
+       set hasStdout 1
+   }
+
+  set testfunc_call ""
+
+  if {${hasStdout} == 0} {
+
+      append testfunc_call "
+
+   {
+      int status;
+
+      status = AxiDMASelfTestExample(${deviceid});
+
+   }"
+  } else {
+
+      append testfunc_call "
+
+   {
+      int status;
+
+
+      print(\"\\r\\n Running AxiDMASelfTestExample() for ${ipname}...\\r\\n\");
+
+      status = AxiDMASelfTestExample(${deviceid});
+
+      if (status == 0) {
+         print(\"AxiDMASelfTestExample PASSED\\r\\n\");
+      }
+      else {
+         print(\"AxiDMASelfTestExample FAILED\\r\\n\");
+      }
+   }"
+  }
+
+  return $testfunc_call
+}
